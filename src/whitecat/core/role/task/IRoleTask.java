5ed9b0d1b920ee5b35d6b428c22c993b00b8be37
Luca Ferrari <fluca1978@gmail.com>
2010-07-21 22:04:40 +0200

    Created the role task abstraction. The idea is that a role can have one or more tasks, even nested together to build complex tasks. Tasks can be configured with parameters and, most important, can be executed. Executing a task means that a role operation/action will be run on the role itself. In the case of nested tasks, each task will be executed in the order they were added as subtasks.

---
+++ src/whitecat/core/role/task/IRoleTask.java
@@ -0,0 +1,89 @@
+/* 
+ * WhiteCat - A dynamic role injector for agents.
+ *
+ * This project represents a new implementation of the so called BlackCat,
+ * a project I made during my thesis degree. For more information about such project please see:
+ * 
+ *   G., L. Ferrari, L. Leonardi,
+ *   Injecting Roles in Java Agents Through Run-Time Bytecode Manipulation
+ *   IBM Systems Journal, Vol. 44, No. 1, pp.185-208, 2005
+ *
+ * This new approach exploits a completely different implementation, keeping the
+ * same idea of BlackCat.
+ * 
+ *
+ * Copyright (C) Luca Ferrari 2008-2010 - cat4hire@users.sourceforge.net
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package whitecat.core.role.task;
+
+import whitecat.core.WCException;
+
+/**
+ * A role task is an operation associated to a role that can be used to achieve
+ * the role aim(s). The idea is that each role has one or more tasks associated to it
+ * that can be used to dynamically execute one or more role operations.
+ * Tasks can be nested together, so that complex tasks (i.e., complex operations)
+ * can be executed at once.
+ * A task can be configured (if needed) adding, one at the time, all the parameters
+ * (values) that must be used at the execution time.
+ * @author Luca Ferrari - cat4hire (at) sourceforge.net
+ *
+ */
+public interface IRoleTask {
+
+    /**
+     * Executes this task (and all its sub-tasks) returning the result.
+     * @return the result of the task (and subtasks) execution
+     * @throws WCException if something goes wrong with the task execution
+     */
+    public Object execute() throws WCException;
+    
+    
+    /**
+     * Adds a parameter for the task execution. This method should be used to prepare
+     * parameters before the task is effectively executed.
+     * Please note that this method must be called with the right sequence in order
+     * to respect the sequence of parameters. This means that if you have to add, for instance,
+     * two parameters called <i>par1</i> and <i>par2</i> you should use this method as follows:
+     * <br>
+     * <code>
+     * task.addTaskParameter( par1 );
+     * task.addTaskParameter( par2 );
+     * task.execute();
+     * </code>
+     * @param parameter the parameter to add
+     */
+    public void addTaskParameter( Object parameter );
+    
+    
+    /**
+     * Resets all the parameters, and allows the preparation of this task for
+     * another execution.
+     */
+    public void resetParameters();
+    
+    
+    /**
+     * Adds another task to this one, so that it will be executed along with the
+     * main task. This allows the task to be composed by several simpler tasks.
+     * Please note that there should be a check to avoid loops and adding several times
+     * the same task.
+     * @param toAdd the task to add to this one
+     * @return true if the sub task has been added, false otherwise
+     */
+    public boolean addSubTask( IRoleTask toAdd );
+}
---
+++ src/whitecat/core/role/task/MethodTaskExecutor.java
@@ -0,0 +1,156 @@
+/* 
+ * WhiteCat - A dynamic role injector for agents.
+ *
+ * This project represents a new implementation of the so called BlackCat,
+ * a project I made during my thesis degree. For more information about such project please see:
+ * 
+ *   G., L. Ferrari, L. Leonardi,
+ *   Injecting Roles in Java Agents Through Run-Time Bytecode Manipulation
+ *   IBM Systems Journal, Vol. 44, No. 1, pp.185-208, 2005
+ *
+ * This new approach exploits a completely different implementation, keeping the
+ * same idea of BlackCat.
+ * 
+ *
+ * Copyright (C) Luca Ferrari 2008-2010 - cat4hire@users.sourceforge.net
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package whitecat.core.role.task;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import whitecat.core.WCException;
+import whitecat.core.role.IRole;
+
+/**
+ * A IRoleTask executor that performs a task as a single method call.
+ * This executor can be used with Java reflection, passing the method object
+ * to use as a task.
+ * @author Luca Ferrari - cat4hire (at) sourceforge.net
+ *
+ */
+public class MethodTaskExecutor implements IRoleTask, IRole {
+
+    /**
+     * The method to execute as task.
+     */
+    private Method methodToExecute = null;
+    
+    
+    /**
+     * Collection of uniques tasks to execute within this one.
+     */
+    private Set<IRoleTask> subTasks = null;
+    
+    /**
+     * A list of parameters to use when executing this task.
+     */
+    private List parameters = null;
+    
+    
+    /**
+     * Default constructor.
+     */
+    public MethodTaskExecutor(){
+	super();
+	this.parameters = new LinkedList();
+	this.subTasks   = new HashSet<IRoleTask>();
+    }
+    
+    
+    
+    /**
+     * Provides the value of the methodToExecute field.
+     * @return the methodToExecute
+     */
+    public synchronized final Method getMethodToExecute() {
+        return this.methodToExecute;
+    }
+
+    /**
+     * Sets the value of the methodToExecute field as specified
+     * by the value of methodToExecute.
+     * @param methodToExecute the methodToExecute to set
+     */
+    public synchronized final void setMethodToExecute(Method methodToExecute) {
+        this.methodToExecute = methodToExecute;
+    }
+
+    /* (non-Javadoc)
+     * @see whitecat.core.role.task.IRoleTask#execute()
+     */
+    public Object execute() throws WCException {
+	// check arguments
+	if( this.methodToExecute == null )
+	    throw new WCException("Cannot execute a task without the method to invoke!");
+	
+	
+	try {
+	    // execute the method: if there are not subtasks, do only this,
+	    // otherwise invoke all the subtasks and keep them in an array
+	    if( this.subTasks.size() == 0 )
+		return this.methodToExecute.invoke( this.parameters.toArray() );
+	    else{
+		Object[] returns = new Object[ this.subTasks.size() + 1 ];
+		returns[0] = this.methodToExecute.invoke( this.parameters.toArray() );
+		
+		Iterator<IRoleTask> iter = this.subTasks.iterator();
+		int i = 1;
+		while( iter.hasNext() ){
+		    returns[i] = iter.next().execute();
+		    i++;
+		}
+		    
+		
+		// all done
+		return returns;
+	    }
+	} catch (IllegalArgumentException e) {
+	    throw new WCException("Wrong number of parameters or type!");
+	} catch (IllegalAccessException e) {
+	    throw new WCException("Cannot invoke the method!");
+	} catch (InvocationTargetException e) {
+	    throw new WCException("Cannot invoke the method on the target !");
+	}
+    }
+
+    /* (non-Javadoc)
+     * @see whitecat.core.role.task.IRoleTask#addSubTask(whitecat.core.role.task.IRoleTask)
+     */
+    public synchronized final boolean addSubTask(IRoleTask toAdd) {
+	if( ! this.subTasks.contains(toAdd) ){
+	    this.subTasks.add(toAdd);
+	    return true;
+	}
+	else
+	    return false;
+    }
+
+    public synchronized final void addTaskParameter(Object parameter) {
+	this.parameters.add( parameter );
+    }
+
+    public synchronized final void resetParameters() {
+	this.parameters.clear();
+    }
+
+}
